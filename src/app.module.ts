4149e99f44ebc144d84afd046a5686358b975521
Cristian Perez <cperezlecaros@gmail.com>
2024-12-16 16:51:16 -0700

    Build product entity and create dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true, //carga automaticamente las entidades
       synchronize: true, //sincroniza cambios en entidades de db
     }),
+    ProductsModule,
   ],
 })
 export class AppModule {}
---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,31 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+//Representacion de objeto en base de datos
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', {
+    unique: true, //constraint
+  })
+  title: string;
+
+  @Column('numeric', { default: 0 })
+  price: number;
+
+  @Column({ type: 'text', nullable: true })
+  description: string;
+
+  @Column('text', { unique: true })
+  slug: string;
+
+  @Column('int', { default: 0 })
+  stock: number;
+
+  @Column('text', { array: true })
+  sizes: string[];
+
+  @Column('text')
+  gender: string;
+}
