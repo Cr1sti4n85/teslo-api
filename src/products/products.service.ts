40a2b3696275d48e5d345a18d3da7da18d7c1e28
Cristian Perez <cperezlecaros@gmail.com>
2024-12-18 16:52:18 -0700

    Implement find by title

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -9,8 +9,8 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { validate as isUUID } from 'uuid';
 import { Product } from './entities/product.entity';
-import { isUUID } from 'class-validator';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
@@ -60,20 +60,28 @@ export class ProductsService {
   async findOne(val: string) {
     let product: Product;
 
-    if (!product && isUUID(val))
+    if (!product && isUUID(val)) {
       product = await this.productRepository.findOneBy({ id: val });
-    // let product: Product;
-    // //uuid
-    // if (!product && isUUID(val)) {
-    //   return await this.productRepository.findOneBy({ val });
-    // }
-
-    if (!isUUID(val)) {
-      product = await this.productRepository.findOne({ where: { slug: val } });
+    } else {
+      //el query builder va a buscar por title o slug. Convertira el title
+      //para no generar problemas con el apostrofe o espacios en blanco
+      const queryBuilder = this.productRepository.createQueryBuilder();
+      product = await queryBuilder
+        .where(`UPPER(title) =:title or slug =:slug`, {
+          title: val.toUpperCase(),
+          slug: val.toLowerCase(),
+        })
+        .getOne();
     }
 
+    // if (!isUUID(val)) {
+    //   product = await this.productRepository.findOne({ where: { slug: val } });
+    // }
+
     if (!product)
-      throw new NotFoundException(`Product with id or slug ${val} not found`);
+      throw new NotFoundException(
+        `Product with id, title or slug ${val} not found`,
+      );
 
     return product;
   }
