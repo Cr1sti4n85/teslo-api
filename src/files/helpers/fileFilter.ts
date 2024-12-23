0430f93abeefa5cb9e1e621ca334148e69ca8917
Cristian Perez <cperezlecaros@gmail.com>
2024-12-23 14:14:28 -0700

    Implement get image url

--- src/files/helpers/fileFilter.ts
+++ src/files/helpers/fileFilter.ts
@@ -1,13 +1,13 @@
+import { BadRequestException } from '@nestjs/common';
+
 //este necesita la request, el filename y una callback
 export const fileFilter = (
   _req: Express.Request,
   file: Express.Multer.File,
   cb: Function,
 ) => {
-  if (!file) return cb(new Error('No file found'), false);
-
   if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-    return cb(new Error('Only image files are allowed!'), false);
+    return cb(new BadRequestException('Only image files are allowed!'), false);
   }
   cb(null, true);
 };
